74d9e1c8526a3396f7a73a1233f6fffe91485751
sofia g <110646163+sofigo1010@users.noreply.github.com>
2024-05-23 05:02:43 -0600

    pikkuli

--- webportfoliosofiag/tailwind.config.mjs
+++ webportfoliosofiag/tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(to bottom, oklch(28.94% 0.081 230), oklch(13.65% 0.091 264.07))",
+      },
+    },
+  },
+  plugins: [],
+};
